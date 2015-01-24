fcb64c459f581de4f7207ce98ce5ee27fe3971d1
Thanh Nguyen <congthanh991@gmail.com>
2015-01-24 21:38:53 +0700

    init project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+RealtimeChat::Application.config.secret_key_base = '210575fd49593be3a978abe70c9d60827ac068c5b9076a23770e619d896b9b9a4074187c417dcc6a5b5a9cf15d1231846ec88b59c61b4fde285f076c4fd3310f'
